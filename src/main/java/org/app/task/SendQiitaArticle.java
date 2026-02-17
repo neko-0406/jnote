df4fd358833e3fa1a00f53e9641f48511b4c2f94
ryoya <sateraito3696@gmail.com>
2026-02-17 19:42:34 +0900

    task発行用の関数を追加

--- src/main/java/org/app/task/SendQiitaArticle.java
+++ src/main/java/org/app/task/SendQiitaArticle.java
@@ -1,9 +1,11 @@
 package org.app.task;
 
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -11,18 +13,19 @@ import okhttp3.Response;
 import org.app.Main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SendQiitaArticle {
     private final OkHttpClient okHttpClient;
-    private String endPoint;
+    private final String endPoint;
 
     public SendQiitaArticle() {
         this.okHttpClient = new OkHttpClient();
         this.endPoint = Main.getAppConfig().getQiitaAPIEndPoint();
     }
 
-    public void fetchArticle() {
+    private List<QiitaArticle> fetchArticle() {
         HttpUrl url = HttpUrl.get(this.endPoint).newBuilder()
                 .addQueryParameter("page", "1")
                 .addQueryParameter("per_page", "5")
@@ -32,17 +35,44 @@ public class SendQiitaArticle {
                 .url(url)
                 .build();
 
+        List<QiitaArticle> article = List.of();
+
         try (Response response = okHttpClient.newCall(request).execute()) {
             if (response.isSuccessful()) {
                 Gson gson = new Gson();
                 var listType = new TypeToken<List<QiitaArticle>>(){}.getType();
-                List<QiitaArticle> article = gson.fromJson(response.body().string(), listType);
-                for (QiitaArticle item : article) {
-                    System.out.println(item);
-                }
+                article = gson.fromJson(response.body().string(), listType);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return article;
+    }
+
+    public void scheduleSendArticleToChannel(JDA jda) {
+        List<QiitaArticle> articleList = this.fetchArticle();
+        TextChannel channel = jda.getTextChannelById(Main.getAppConfig().getQiitaChannelId());
+
+        if (articleList.isEmpty()) {
+            MessageEmbed messageEmbed = new EmbedBuilder()
+                    .setTitle("Qiitaの記事の取得に失敗しました")
+                    .build();
+            channel.sendMessageEmbeds(messageEmbed).queue();
+            return;
+        }
+
+        List<MessageEmbed> articleEmbeds = new ArrayList<>();
+        for (QiitaArticle item: articleList) {
+            articleEmbeds.add(
+                    new EmbedBuilder()
+                            .setTitle(item.getTitle())
+                            .setDescription(item.getBody().substring(0, 100) + "...")
+                            .setUrl(item.getUrl())
+                            .setFooter("create at:" + item.getCreateAt())
+                            .build()
+            );
+        }
+        channel.sendMessageEmbeds(articleEmbeds).queue();
     }
 }
