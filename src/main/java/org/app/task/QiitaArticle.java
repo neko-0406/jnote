47b64e00f3e5af77b52dee241068e397fb60733b
ryoya <sateraito3696@gmail.com>
2026-02-17 19:03:43 +0900

    必要な部分のみ取得するように変更

--- src/main/java/org/app/task/QiitaArticle.java
+++ src/main/java/org/app/task/QiitaArticle.java
@@ -1,132 +1,26 @@
 package org.app.task;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
 
-import java.util.List;
-
+@Getter
 public class QiitaArticle {
-    @SerializedName("rendered_body")
-    private String renderedBody;
-
-    private String body;
-
-    private boolean coediting;
-
-    @SerializedName("comments_count")
-    private int commentsCount;
-
-    @SerializedName("created_at")
-    private String createdAt;
-
-    private Group group;
-
     private String id;
-
-    @SerializedName("likes_count")
-    private int likesCount;
-
-    @SerializedName("private")
-    private boolean isPrivate; // 'private'は予約語のため別名に
-
-    @SerializedName("reactions_count")
-    private int reactionsCount;
-
-    @SerializedName("stocks_count")
-    private int stocksCount;
-
-    private List<Tag> tags;
-
     private String title;
-
-    @SerializedName("updated_at")
-    private String updatedAt;
-
+    private String body;
     private String url;
-
-    private User user;
-
-    @SerializedName("page_views_count")
-    private int pageViewsCount;
-
-    @SerializedName("team_membership")
-    private TeamMembership teamMembership;
-
-    @SerializedName("organization_url_name")
-    private String organizationUrlName;
-
-    private boolean slide;
-
-    // --- 内部クラス定義 (static class) ---
-
-    public static class Group {
-        @SerializedName("created_at")
-        private String createdAt;
-
-        private String description;
-
-        private String name;
-
-        @SerializedName("private")
-        private boolean isPrivate;
-
-        @SerializedName("updated_at")
-        private String updatedAt;
-
-        @SerializedName("url_name")
-        private String urlName;
-    }
-
-    public static class Tag {
-        private String name;
-        private List<String> versions;
-    }
-
-    public static class User {
-        private String description;
-
-        @SerializedName("facebook_id")
-        private String facebookId;
-
-        @SerializedName("followees_count")
-        private int followeesCount;
-
-        @SerializedName("followers_count")
-        private int followersCount;
-
-        @SerializedName("github_login_name")
-        private String githubLoginName;
-
-        private String id;
-
-        @SerializedName("items_count")
-        private int itemsCount;
-
-        @SerializedName("linkedin_id")
-        private String linkedinId;
-
-        private String location;
-
-        private String name;
-
-        private String organization;
-
-        @SerializedName("permanent_id")
-        private int permanentId;
-
-        @SerializedName("profile_image_url")
-        private String profileImageUrl;
-
-        @SerializedName("team_only")
-        private boolean teamOnly;
-
-        @SerializedName("twitter_screen_name")
-        private String twitterScreenName;
-
-        @SerializedName("website_url")
-        private String websiteUrl;
-    }
-
-    public static class TeamMembership {
-        private String name;
+    @SerializedName(value = "create_at")
+    private String createAt;
+    @Override
+    public String toString() {
+        String temp = "QiitaArticle { id: %s, url: %s, title: %s, body: %s, createAt: %s }";
+        return String.format(
+                temp,
+                this.id,
+                this.url,
+                this.title,
+                this.body,
+                this.createAt
+        );
     }
 }
--- src/main/java/org/app/task/SendQiitaArticle.java
+++ src/main/java/org/app/task/SendQiitaArticle.java
@@ -1,5 +1,9 @@
 package org.app.task;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -7,6 +11,7 @@ import okhttp3.Response;
 import org.app.Main;
 
 import java.io.IOException;
+import java.util.List;
 
 public class SendQiitaArticle {
     private final OkHttpClient okHttpClient;
@@ -29,7 +34,12 @@ public class SendQiitaArticle {
 
         try (Response response = okHttpClient.newCall(request).execute()) {
             if (response.isSuccessful()) {
-
+                Gson gson = new Gson();
+                var listType = new TypeToken<List<QiitaArticle>>(){}.getType();
+                List<QiitaArticle> article = gson.fromJson(response.body().string(), listType);
+                for (QiitaArticle item : article) {
+                    System.out.println(item);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
